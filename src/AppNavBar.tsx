718bf9ec1739a6d3f633a11f8a7063e41c57017b
Andrey_Shleger <Andrey_Shleger@epam.com>
2021-05-09 00:47:50 +0300

    onClickMenu

--- src/AppNavBar.tsx
+++ src/AppNavBar.tsx
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Menu, Fade, MenuItem } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -25,13 +26,44 @@ const useStyles = makeStyles((theme: Theme) =>
 export default function ButtonAppBar() {
   const classes = useStyles();
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            id="fade-button"
+            aria-controls="fade-menu"
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+            edge="start" className={classes.menuButton}
+            color="inherit"
+            aria-label="menu">
             <MenuIcon />
           </IconButton>
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              'aria-labelledby': 'fade-button',
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem onClick={handleClose}>Profile1</MenuItem>
+            <MenuItem onClick={handleClose}>My account2</MenuItem>
+            <MenuItem onClick={handleClose}>Logout3</MenuItem>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
             News123
           </Typography>
