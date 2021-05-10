a19e46ebcffce11bb270f04d4a985f7af5dcb24a
Andrey_Shleger <Andrey_Shleger@epam.com>
2021-05-10 10:10:06 +0300

    save into localStorage onInput

--- src/cards/Profile.tsx
+++ src/cards/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { Typography } from '@material-ui/core';
@@ -32,22 +32,39 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function SimplePaper() {
   const classes = useStyles();
+  
+  const [address, setAddress] = useState("")
+  const [secret, setSecret] = useState("")
+  const [user, setUser] = useState("")
+
+  useEffect(() => {
+    document.title = `You clicked ${address} times`;
+    localStorage.setItem('accAddress', address)
+    localStorage.setItem('accSecret', secret)
+    localStorage.setItem('accUser', user)
+  });
+
+
+
 
   return (
     <div className={classes.root} >
       <Paper elevation={3} >
         <Typography variant="h5" gutterBottom>Testnet credentials</Typography>
         <form className={classes.inp} noValidate autoComplete="off">
-          <TextField id="standard-basic" label="Account address" />
-          <TextField id="filled-basic" label="Account secret" variant="filled" />
+          <TextField id="standard-basic" label="Name" onChange= {(e) => setUser(e.target.value)}/>
+          <TextField id="standard-basic" label="Account address" onChange= {(e) => setAddress(e.target.value)} />
+          <TextField id="filled-basic" label="Account secret" type="password" variant="filled" onChange= {(e) => setSecret(e.target.value)}/>
           <Button
-        variant="contained"
-        color="primary"
-        size="large"
-        className={classes.button}
-        startIcon={<SaveIcon />}
-      >
-        Save
+            variant="contained"
+            color="primary"
+            size="large"
+            //just for sure
+            onClick={() => {setAddress(address);setSecret(secret);setUser(user)}}
+            className={classes.button}
+            startIcon={<SaveIcon />}
+          >
+            Save
       </Button>
         </form>
       </Paper>
